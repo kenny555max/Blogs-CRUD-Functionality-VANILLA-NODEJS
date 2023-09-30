55b3675ecc63dab6ff6936463ef2f8685a023f45
nice <oyedepokehinde2016@gmail.com>
2023-09-30 17:50:45 +0100

    first commit

--- app.js
+++ app.js
@@ -1,85 +1,92 @@
-//set up a express app
 const express = require('express');
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const app = express();
 
-//set up a view a view engine
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(
-    express.urlencoded({ extended: true })
-);
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-//set up a single route
-// http://localhost:5000/
+//sample route
+//http:localhost:5000
 app.get('/', (req, res) => {
     res.redirect('/blogs');
 });
 
-// http://localhost:5000/blogs
-app.get('/blogs', (req, res) => {
-    res.render('blogs.ejs');
-});
 
-// http://localhost:5000/about
-app.get('/about', (req, res) => {
-    res.render('about.ejs');
-});
-
-//model
+//schema
 const Schema = new mongoose.Schema({
     blog_title: String,
     blog_snippet: String,
     blog_body: String,
-    created_At: {
-        default: Date.now(),
-        type: Date
+    blog_created_at: {
+        type: Date,
+        default: Date.now //current date
     }
 });
 
-const Model = mongoose.model('blogs', Schema);
+//model
+const Model = mongoose.model('Blogs', Schema);
 
-// http://localhost:5000/new_blog
-app.get('/new_blog', (req, res) => {
-    let error = '';
-
-    if (req.query.error) {
-        error = req.query.error; //true
-    }
-
-    res.render('new_blog.ejs', { error });
-});
-app.post('/create/blog', async (req, res) => {
+// http:localhost:5000/blogs
+app.get('/blogs', async (req, res) => {
     try {
-        console.log(req.body);
-        if (req.body.blog_title === '' || req.body.blog_snippet === '' || req.body.blog_body === '') {
-            res.redirect(`/new_blog?error=${true}`);
-        }
+        const blogs = await Model.find();
 
-        //save the data to the database
-        const blog = await Model.create(
-            {
-                blog_title: req.body.blog_title,
-                blog_snippet: req.body.blog_snippet,
-                blog_body: req.body.blog_body
+        //console.log(blogs);
+        //Map - hihger order array method/function - which returns a new array
+        const all_blogs = blogs.map(function (blog) {
+            return {
+                blog_id: blog._id,
+                blog_title: blog.blog_title,
+                blog_snippet: blog.blog_snippet,
+                blog_body: blog.blog_body,
+                blog_created_at: moment(blog.blog_created_at).calendar() // Today at 11:55
             }
-        );
+        });
 
-        console.log(blog);
-
-        res.redirect('/dashboard');
+        res.render('blogs', { blogs: all_blogs });
     } catch (error) {
         console.log(error);
     }
 });
 
-//create a listener
+//http:localhost:5000/blogs/about - render the about.ejs
+// GET, POST, DELETE, PUT
+app.get('/blogs/about', (req, res) => {
+    res.render('about');
+});
+
+//http:localhost:5000/blogs/new_blog - render the new_blog.
+app.get('/blogs/new_blog', (req, res) => {
+    console.log(req.query);
+
+    res.render('new_blog');
+});
+
+//http://localhost:5000/blogs/create_blog
+app.post('/blogs/create_blog', async (req, res) => {
+    try {
+        const blog = await Model.create(req.body);
+
+        console.log(blog);
+
+        res.redirect('/blogs');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get('/blogs/edit/:id', (req, res) => {
+    res.redirect(`/blogs/new_blog?blog_id=${req.params.id}`);
+});
+
 const PORT = 5000;
 
-const CONNECTION_URI = 'mongodb+srv://blog:blog@cluster0.kyxpvui.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URI = 'mongodb+srv://mongodbclass:mongodb@cluster0.vufqv.mongodb.net/?retryWrites=true&w=majority';
 
-//mongodb connection here
-mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`)))
+//listener
+mongoose.connect(CONNECTION_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`)))
     .catch((error) => console.log(error));
